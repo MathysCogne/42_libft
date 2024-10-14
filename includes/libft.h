e77479c0895069442158dc40d14ab63dc9435e2f
Mathys Cogne <mathys.cognef@gmail.com>
2024-10-14 19:35:00 +0200

    ft_put*_fd

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: mcogne-- <mcogne--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 17:35:20 by mcogne--          #+#    #+#             */
-/*   Updated: 2024/10/14 19:04:52 by mcogne--         ###   ########.fr       */
+/*   Updated: 2024/10/14 19:34:00 by mcogne--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,18 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+# define INT_MAX 2147483647
+# define INT_MIN -2147483648
+
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *mem, int c, size_t size);
 void	*ft_calloc(size_t count, size_t size);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 char	*ft_strcpy(char *dest, const char *src);
 char	*ft_strdup(const char *src);
